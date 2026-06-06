48858265114d97fffe987a42a2e25aa7de4a5daa
joan purba <joanpurba562@gmail.com>
2026-06-07 00:29:55 +0700

    delete: delete prisma config file

--- prisma/prisma.config.ts
+++
@@ -1,9 +0,0 @@
-// prisma/prisma.config.ts
-import { defineConfig } from "prisma";
-
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-export default defineConfig({
-  datasource: {
-    url: process.env.DATABASE_URL,
-  },
-});
