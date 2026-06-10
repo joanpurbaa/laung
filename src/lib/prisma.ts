dbaa30725ce61eb765821832ab27fb18b702cddb
joan purba <joanpurba562@gmail.com>
2026-06-11 04:30:08 +0700

    fix: fix the datasource for prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,21 +7,16 @@ const createPrismaClient = () => {
   const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
   });
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
   const adapter = new PrismaPg(pool);
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment
   return new PrismaClient({ adapter });
 };
 
 const globalForPrisma = globalThis as unknown as {
-  // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
   prisma: ReturnType<typeof createPrismaClient> | undefined;
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 export const db = globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   globalForPrisma.prisma = db;
 }
